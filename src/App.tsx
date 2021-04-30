e06f77fab63124e186bef7cb96aae62188558fca
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-04-29 21:26:34 -0300

    add removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState,useRef } from "react";
 type FormElement = React.FormEvent<HTMLFormElement>;
 interface ITask {
   name: string;
@@ -7,36 +7,72 @@ interface ITask {
 function App(): JSX.Element {
   const [newTask, setNewTask] = useState<string>("");
   const [tasks, setTasks] = useState<ITask[]>([]);
+  const taskInput=useRef<HTMLInputElement>(null);
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
     addTask(newTask);
     console.log(tasks);
     setNewTask("");
+    taskInput.current?.focus();
   };
-  const addTask = (name: string) => {
+  const addTask = (name: string): void => {
     const newTasks: ITask[] = [...tasks, { name, done: false }];
     setTasks(newTasks);
   };
+  const toggleDondeTask = (index: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[index].done = !newTasks[index].done;
+    setTasks(newTasks);
+  };
+  const removeTask = (index: number): void => {
+    const newTasks: ITask[]=[...tasks];
+    newTasks.splice(index,1);
+    setTasks(newTasks);
+  };
   return (
-    <Fragment>
-      <div className="card">
-        <div className="card-body">
-          <form onSubmit={handleSubmit}>
-            <input
-              className="form-control"
-              type="text"
-              value={newTask}
-              onChange={(e) => setNewTask(e.target.value)}
-            />
-            <button className="btn btn-primary">Save</button>
-          </form>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  ref={taskInput}
+                  className="form-control"
+                  type="text"
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">Save</button>
+              </form>
+            </div>
+          </div>
+
+          {tasks.map((task: ITask, index: number) => (
+            <div className="card card-body mt-2" key={index}>
+              <h2 style={{ textDecoration: task.done ? "line-through" : "" }}>
+                {task.name}
+              </h2>
+              <div>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => toggleDondeTask(index)}
+                >
+                  {task.done ? "✓" : "✗"}
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeTask(index)}
+                >
+                  🗑
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-
-      {tasks.map((task: ITask, index: number) => (
-        <h1 key={index}>{task.name}</h1>
-      ))}
-    </Fragment>
+    </div>
   );
 }
 
